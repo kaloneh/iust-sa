3ecb92659643cb04878efffbe8f76245032610da
Kamran Hayati <kaloneh@gmail.com>
2023-01-24 08:15:43 +0330

    add IustSAApplication.java entry point

---
+++ src/main/java/ac/ir/iust/sa/PlatformApplication.java
@@ -0,0 +1,13 @@
+package ac.ir.iust.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IustSAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformApplication.class, args);
+	}
+
+}
