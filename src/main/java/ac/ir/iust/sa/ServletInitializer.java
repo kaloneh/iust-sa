c65588737e60e6336f2afa9de9b5d9cf1bc74796
Kamran Hayati <kaloneh@gmail.com>
2023-01-24 08:17:19 +0330

    add ServletInitializer.java

---
+++ src/main/java/ac/ir/iust/sa/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ac.ir.iust.sa;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PlatformApplication.class);
+	}
+
+}
